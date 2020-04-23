0836fa84d0a9d99c2db66c150b7cb5b1d034e962
slucter <slucter133@gmail.com>
2020-04-23 17:38:50 +0700

    Navbar

---
+++ src/components/Navbar/NavCenter.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="navbar-w">
+        <Logo_ />
+        <Category_ />
+        <Search_ />
+        <IconGroup_ />
+        <UserSection_ />
+    </div>
+</template>
+
+<script>
+import Logo_ from './NavCenterComponents/_Logo.vue';
+import Category_ from './NavCenterComponents/_Category.vue';
+import Search_ from './NavCenterComponents/_Search.vue';
+import IconGroup_ from './NavCenterComponents/_Icon_group.vue';
+import UserSection_ from './NavCenterComponents/_User_section.vue';
+
+export default {
+  name: 'NavCenter',
+  components: {
+    Logo_,
+    Category_,
+    Search_,
+    IconGroup_,
+    UserSection_,
+  },
+};
+</script>
+
+<style scoped>
+.navbar-w{
+    display: flex;
+    background-color: rgb(255, 255, 255);
+    width: 97%;
+    height: 56px;
+    margin: 0 auto;
+}
+</style>
---
+++ src/components/Navbar/NavCenterComponents/_Category.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="nav-cat">
+        <a href="#">Kategori</a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: '_Category',
+
+};
+</script>
+
+<style scoped>
+.nav-cat{
+    display: flex;
+    flex: .5;
+    background-color: rgb(255, 255, 255);
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+}
+.nav-cat a{
+    text-decoration: none;
+    font-weight: 500;
+    color: #5a5a5a;
+}
+</style>
