37ebcaa545678623e9250953903f4f27503437bc
slucter <slucter133@gmail.com>
2020-04-23 21:43:42 +0700

    fix

---
+++ src/components/Navbar/Submenu/SubmenuComponents/_SubmenuSection.vue
@@ -0,0 +1,28 @@
+<template>
+      <div class="submenu-belanja-section">
+          <ScrollMenu_ />
+          <SubmenuRight_ />
+      </div>
+</template>
+
+<script>
+import ScrollMenu_ from './_ScrollMenu.vue';
+import SubmenuRight_ from './_SubmenuRight.vue';
+
+export default {
+  name: 'BelanjaSection',
+  components: {
+    ScrollMenu_,
+    SubmenuRight_,
+  },
+};
+</script>
+
+<style scoped>
+.submenu-belanja-section{
+    width: 100%;
+    height: 453px;
+    background-color: #fff;
+    display: flex;
+}
+</style>
